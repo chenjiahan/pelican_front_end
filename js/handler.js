b9723dc80bd81a61ca6abb057eb0982e73aeb850
chenjiahan <416417567@163.com>
2015-07-06 19:56:07 +0800

    iframe修正

--- js/handler.js
+++ js/handler.js
@@ -357,6 +357,7 @@
                     obj.data.id = id;
                     obj.data.receivedDate = obj.data.receivedDate.substr(0,16).replace('-','年').replace('-','月').replace('T','日 ');
                     document.getElementById('task').innerHTML = template('md-tmpl',obj);
+                    $('#detail-iframe').innerHTML = obj.data.html;
                 } else {
                     topAlert('网络错误','error');
                 }
